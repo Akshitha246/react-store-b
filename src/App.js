7b0acef2bbb3f130a0556537f97117d8c302fe13
Akshitha246 <akshitha6akki@gmail.com>
2025-03-19 12:17:21 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Products from "./components/Products";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route index element={<Products/>}/>
+        <Route path="home" element={<Products/>}/>
+        <Route path="Cart" element={<Cart/>}/>
+      </Routes>
+      <Products/>
+      <Footer/>
+    </BrowserRouter>
   );
 }
-
-export default App;
+ export default App;
---
+++ src/component/header.jsx
@@ -0,0 +1,10 @@
+import "./Header.css"
+import {link} from "react-router-dom"
+export default function Header(){
+    return(
+        <div className="App-Header-Row">
+            <div>React Store</div>
+            <div>Menu</div>
+        </div>
+    )
+}
